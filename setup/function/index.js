67d827ea051d962e0ebd8bf45bbbaeee5f733206
classic-code <50813314+codeBlock25@users.noreply.github.com>
2020-11-24 16:56:20 +0100

    Update index.js

--- setup/function/index.js
+++ setup/function/index.js
@@ -1,23 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -60,7 +41,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.shuffle = exports.FindWinnerOnMatcher = exports.FindWinnerOnRoshambo = exports.MarkRoshamboGame = exports.PlayerDrawCash = exports.PlayerCoinLeft = exports.PlayerCashLeft = exports.PlayerCash = exports.AdminCash = exports.FindWinnerOnPenalty = exports.PlayAdmin = exports.NotificationAction = void 0;
 var admin_model_1 = __importDefault(require("../model/admin_model"));
-var notification_1 = __importStar(require("../model/notification"));
+var notification_1 = __importDefault(require("../model/notification"));
 var plays_1 = require("../model/plays");
 var enum_1 = require("../types/enum");
 exports.NotificationAction = {
@@ -73,7 +54,7 @@ exports.NotificationAction = {
                             $push: {
                                 notifications: {
                                     message: message,
-                                    type: type !== null && type !== void 0 ? type : notification_1.notificationType.game,
+                                    type: type !== null && type !== void 0 ? type : enum_1.notificationHintType.win,
                                     time: new Date(),
                                     hasNew: true,
                                 },
